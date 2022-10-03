6086f6585d0288a89837ab560c1d9711828a2429
ChousX <gmanenergy@gmail.com>
2022-10-03 01:30:07 -0400

    init

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@ pub struct Mob;
 #[derive(Default)]
 pub struct MobBuilder {
     position: Option<(f32, f32)>,
-    size: Option<f32>,
+    health: Option<u16>,
+    speed: Option<f32>,
 }
 
 impl MobBuilder {
@@ -23,8 +24,13 @@ impl MobBuilder {
         self
     }
 
-    pub fn add_size(&mut self, size: f32) -> &mut Self {
-        self.size = Some(size);
+    pub fn add_health(&mut self, health: u16) -> &mut Self {
+        self.health = Some(health);
+        self
+    }
+
+    pub fn add_speed(&mut self, speed: f32) -> &mut Self{
+        self.speed = Some(speed);
         self
     }
 
@@ -51,10 +57,35 @@ pub struct SeenBuilder {
 }
 
 impl SeenBuilder {
+    pub fn add_camera_position(&mut self, x: f32, y: f32, z: f32) -> &mut Self{
+        self.camera_position = Some((x, y, z));
+        self
+    }
+
+    pub fn add_camera_looking_at(&mut self, target: Vec3, up: Vec3) -> &mut Self{
+        self.camera_looking_at = Some((target, up));
+        self
+    }
+
     fn get_camera_transform(&self) -> Transform {
         let (x, y, z) = self.camera_position.unwrap_or((-2.0, 2.5, 5.0));
         let (target, up) = self.camera_looking_at.unwrap_or((Vec3::ZERO, Vec3::Y));
         Transform::from_xyz(x, y, z).looking_at(target, up)
+    }
+
+    pub fn add_light_intensity(&mut self, intensity: f32) -> &mut Self{
+        self.light_intensity = Some(intensity);
+        self
+    }
+
+    pub fn add_shadows(&mut self, shadows: bool) -> &mut Self{
+        self.shadows = shadows;
+        self
+    }
+
+    pub fn add_light_position(&mut self, x: f32, y: f32, z: f32) -> &mut Self{
+        self.light_position = Some((x, y, z));
+        self
     }
 
     fn get_light_pos(&self) -> Transform {
@@ -70,6 +101,12 @@ impl SeenBuilder {
         self.plane_size = Some(size);
         self
     }
+
+    pub fn add_(&mut self, size: f32) -> &mut Self {
+        self.plane_size = Some(size);
+        self
+    }
+
 
     pub fn add_position(&mut self, x: f32, y: f32, z: f32) -> &mut Self {
         self.plane_position = Some((x, y, z));
